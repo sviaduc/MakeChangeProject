df4be1f381aecb89c6f0adde1b6943720ad1f3d3
Stanton Viaduc <stan@Stantons-MacBook-Pro.local>
2017-08-05 19:06:30 -0600

    added array and adjust change loop

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -5,6 +5,11 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		
+		int[] denom = {2000, 1000, 500, 100, 25, 10, 5, 1};
+		String[] bill = {"twenty dollar bill", "ten dollar bill", "five dollar bill", "one dollar bill", 
+		"quarters", "dimes", "nickels", "pennies"};
+		int amount;
+		
 		System.out.println("What is the price of the item?");
 		double price = kb.nextDouble();
 		
@@ -24,8 +29,20 @@ public class MakeChange {
 		//list of possible denominations 
 		
 		// division done to find remainder
-		double change = ((100*tender) - (100*price))/100 ;
+		double change = ((100*tender) - (100*price)) ;
 		System.out.println(change);
+		
+		// given the the change, it needs to be put into denominations.
+		//need a loop? to repeat denominations if needed.
+		
+		
+		for(int i = 0; i <denom.length; i++ ) {
+			amount = (int)(change/denom[i]);
+			change = change%denom[i];
+			
+			System.out.println(amount + " " + bill[i]);
+		}
+	
 				
 
 	}
